e8a86181716388f05d81427d818b1b27c3cc0561
venkatvignes <venkatvjn007@gmail.com>
2023-11-27 19:37:16 +0530

    facebook login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import Home from "./Components/home";
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import FacebookLogin from 'react-facebook-login';
 
 function App() {
+  const responseFacebook = (response) => {
+    console.log(response);
+  }
+   
   return (
     <div className="App">
+   <FacebookLogin
+     appId="1036479691023707"
+     autoLoad={true}
+     fields="name,email,picture"
+     cssClass="my-facebook-button-class"
+     icon="fa-facebook"
+    //  onClick={componentClicked}
+     callback={responseFacebook} />
       <GoogleOAuthProvider clientId="381004058757-e7n42a24o1pst62ch61l2sod8hniojqa.apps.googleusercontent.com">
           <Home />
         </GoogleOAuthProvider>
